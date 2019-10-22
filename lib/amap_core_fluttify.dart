d50d1f3e660db54089c3dc7df2c158f433cb0fd9
yohom <382146139@qq.com>
2019-10-22 15:06:59 +0800

    roll engine #231fd8f

--- lib/amap_core_fluttify.dart
+++ lib/amap_core_fluttify.dart
@@ -1,3 +1,6 @@
 library amap_core_fluttify;
 
+// 由于core面向的是开发者, 这里把生成的类也全部export出去, 供上层开发者自行使用
+export 'src/android/android.export.g.dart';
 export 'src/dart/amap_core.dart';
+export 'src/ios/ios.export.g.dart';
