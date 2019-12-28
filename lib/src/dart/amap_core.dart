a138458c3102b342897d8cb2e7d32b80541f781e
yohom <yohombao@gmail.com>
2019-12-28 22:32:18 +0800

    enhance: 去掉android的打印信息

--- lib/src/dart/amap_core.dart
+++ lib/src/dart/amap_core.dart
@@ -5,9 +5,7 @@ import '../ios/ios.export.g.dart';
 class AmapCore {
   static Future<void> init(String iosKey) {
     return platform(
-      android: (pool) async {
-        print('android端需要在Manifest里面设置');
-      },
+      android: (pool) async {},
       ios: (pool) async {
         final service = await AMapServices.sharedServices();
         await service.set_apiKey(iosKey);
