b85cc8f64ed4fbeb1620ecef7af832968cfde448
yohom <yohombao@qq.com>
2020-04-17 08:34:32 +0800

    roll engine #10d0447c

--- lib/src/ios/ios.export.g.dart
+++ lib/src/ios/ios.export.g.dart
@@ -1,6 +1,7 @@
 export 'AMapRouteConfig.g.dart';
 export 'AMapDrivingStrategy.g.dart';
 export 'type_op.g.dart';
+export 'constants.g.dart';
 export 'AMapNaviConfig.g.dart';
 export 'AMapServices.g.dart';
 export 'functions.g.dart';
