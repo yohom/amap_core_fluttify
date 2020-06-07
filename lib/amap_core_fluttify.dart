1e8bb13a4e2a62c1db3f25e2b3e74e9d616c8276
yohom <382146139@qq.com>
2020-06-07 11:44:44 +0800

    enhance: 提升依赖, 去除LatLng

--- lib/amap_core_fluttify.dart
+++ lib/amap_core_fluttify.dart
@@ -3,5 +3,4 @@ library amap_core_fluttify;
 export 'package:foundation_fluttify/foundation_fluttify.dart';
 
 export 'src/facade/amap_core.dart';
-export 'src/facade/models.dart';
 export 'src/ios/ios.export.g.dart';
